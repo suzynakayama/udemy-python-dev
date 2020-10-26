fe437fd00fe7f1c0b2338bd8a5db05ca5cdc2500
suzynakayama <suzy.nakayama@gmail.com>
2020-10-25 21:13:43 -0400

    numbers

---
+++ 01-Python/01-Basics.py
@@ -0,0 +1,26 @@
+# Numbers
+
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+print(type(2 + 4))    # <class int>
+print(type(2 / 4))    # <class float>
+
+print(2 ** 3)   # 8
+print(5 // 4)   # 1
+print(6 % 4)    # 2
+
+print(round(3.1))   # 3         - rounds the number
+print(abs(-5))      # 5         - returns absolute value (no negative number)
+print(bin(5))       # you get the binary representation after the b     0b101
+print(int('0b101', 2))      # convert from binary to base 2 number      5
+
+# Operator precedence
+# ()
+# **
+# * and /
+# + and -
+
+# Strings
